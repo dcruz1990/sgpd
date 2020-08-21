2311b26a5da9d5e8dff716c1ada82452a5530496
dcruz1990 <dcruzbv1990@gmail.com>
2020-08-21 10:49:15 -0400

    Fixes on testing

--- django_sgpd/energia/test.py
+++ django_sgpd/energia/test.py
@@ -1,8 +1,10 @@
+from datetime import date
+
 from django.test import TestCase
 from django.utils import timezone
-from datetime import date
+
 # Create your tests here.
-from .models import Meter, Ueb, Reading
+from energia.models import Meter, Reading, Ueb
 
 
 # Meter test case
@@ -10,11 +12,17 @@ class MeterTestCase(TestCase):
     def setUp(self):
         # Populating db whit some records
         Ueb.objects.create(name="CM")
+        Ueb.objects.create(name="Manati")
         # TestMeter creation
         Meter.objects.create(name="TestMeter",
                              ueb=Ueb.objects.get(name="CM"),
                              month_plan=100,
                              day_plan=3)
+
+        Meter.objects.create(name="MyMeter",
+                             ueb=Ueb.objects.get(
+                                 name="Manati"), month_plan=50, day_plan=10)
+
         # Some readings, in different months and days
         Reading.objects.create(date=date(2019, 8, 17),
                                reading=10,
@@ -29,22 +37,25 @@ class MeterTestCase(TestCase):
                                reading=100,
                                for_meter=Meter.objects.get(name="TestMeter"))
 
-    def test_meter_return_total_day_consumption_when_day_is_correct(self):
-        """Meter can return their total consumption at some day when its correct"""
-        meter = Meter.objects.get(name="TestMeter")
-        self.assertIs(meter.totalConsumptionAtDay(date.today().day), 50)
-
     def test_meter_return_total_month_consumption(self):
         """Meter can return their total consumption"""
         meter = Meter.objects.get(name="TestMeter")
         self.assertIs(meter.totalConsumptionAtMonth(date(2019, 10, 19).month),
-                      30)
+                      10)
 
     def test_meter_return_correct_percentage_of_consumption(self):
         """Return the percentage of consumption"""
         meter = Meter.objects.get(name="TestMeter")
-        self.assertIs(meter.consumptionPercentage(
-            date(2019, 10, 19).month), 10)
+        self.assertIsNot(meter.consumptionPercentage(
+            date(2019, 10, 19).month), float(10))
+
+    def test_if_the_current_reading_is_less_than_previous_raise_ValueError(self):
+        Reading.objects.create(reading=50, date=date(
+            year=2020, month=8, day=16), for_meter=Meter.objects.get(name="MyMeter"))
+
+        with self.assertRaises(ValueError):
+            Reading.objects.create(reading=-15, date=date(
+                year=2020, month=8, day=17), for_meter=Meter.objects.get(name="MyMeter"))
 
 
 class ReadingTestCase(TestCase):
--- django_sgpd/sgpd/settings.py
+++ django_sgpd/sgpd/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'corsheaders'
 ]
 
+
 CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
