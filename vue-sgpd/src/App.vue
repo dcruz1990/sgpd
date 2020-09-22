191399204622118fe26bf0255316f63d32ed7edc
dcruz1990 <dcruzbv1990@gmail.com>
2020-09-22 02:32:50 -0400

    Added Header

--- vue-sgpd/src/App.vue
+++ vue-sgpd/src/App.vue
@@ -1,8 +1,13 @@
 <template>
+
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <b-container fluid>
+    <Header/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </b-container>
   </div>
+
+  
 </template>
 
 <script>
@@ -19,12 +24,12 @@ export default {
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
