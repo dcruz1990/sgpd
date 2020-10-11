1ab9dfd17e341b5bb426d2698b198c473b1f684e
dcruz1990 <dcruzbv1990@gmail.com>
2020-10-11 02:29:44 -0400

    Now using reverse in the test urls

--- django_sgpd/energia/tests/test_api.py
+++ django_sgpd/energia/tests/test_api.py
@@ -1,18 +1,15 @@
-from os import name
 
 from django.contrib.auth.models import User
 from django.http import response
-from django.http.request import bytes_to_text
-from django.template import context
+
 from django.test import client
 from django.urls.base import reverse
+
 from rest_framework import status
-from rest_framework.authtoken.models import Token
-from rest_framework.test import APIClient, APITestCase
+from rest_framework.test import APITestCase
 
 from energia.models import Meter, Reading, Ueb
 
-import json
 
 
 class MeterTests(APITestCase):
@@ -32,7 +29,7 @@ class MeterTests(APITestCase):
         """
         Ensure we can get meters 
         """
-        url = "/api/meters/"
+        url = reverse('meter-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -44,28 +41,32 @@ class MeterTests(APITestCase):
                 'ueb': 1,
                 'day_plan': 10,
                 'month_plan': 300}
-        url = "/api/meters/"
+        url = reverse('meter-list')
         response = self.client.post(url, data)
         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Meter.objects.filter(name="TestMeter").count(), 1)
 
     def test_get_readings_by_month(self):
         """ Get all readings by given month """
-        url = '/api/meters/1/readings_by_month/?month=10'
+        url = "%s?month=10" % reverse('meter-readings-by-month', kwargs={'pk': 1}) 
+        # ''/api/meters/1/readings_by_month/?month=10'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-
     def test_uebs_endpoint_works(self):
         """ Get all uebs"""
-        url = "/api/uebs/"
+        url = reverse('ueb-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_total_consumption_of_ueb_with_id_1(self): 
         """ Get total consumption of ueb with id 1"""
-        url = "/api/uebs/1/totalconsumption/"
+        url = reverse('ueb-totalconsumption', kwargs={'pk':1})
         response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, { "name" : "TestUeb" , "consumption" : 0 })
+
+    
 
 
 
